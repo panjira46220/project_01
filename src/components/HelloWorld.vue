cc458fcda1c230073871e9a496fc82fe2a0c9e86
panjira46220 <6035512072@psu.ac.th>
2020-05-07 23:06:16 +0700

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,48 +1,114 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+  <div class='hello'>
+    <div>
+      <input type="text" v-model="name" placeholder="NAME">
+      <input type="text" v-model="tel" placeholder="TEL">
+      <input type="text" v-model="gunclass" placeholder="Class">
+      <input type="text" v-model="typegun" placeholder="Competition typeL">
+      <button @click="insertToContact(tel, name,gunclass,typegun)">Add</button>
+    </div>
+
+   
+
+    <ul :key="key" v-for="(contact, key) in contacts">
+      <div v-if="updateKey === key">
+        <input type="text" v-model="updateName" placeholder="NAME">
+        <input type="text" v-model="updateTel" placeholder="TEL">
+        <input type="text" v-model="updateGunclass" placeholder="Class">
+        <input type="text" v-model="updateTypegun" placeholder="Competition typeL">
+        <button @click="updateContact(updateTel, updateName,updateGunclass,updateTypegun)">Save</button>
+      </div>
+      <li v-else>
+        {{contact.name}} : {{contact.tel}} : {{contact.gunclass}} : {{contact.typegun}}
+        <button @click="setUpdateContact(key, contact)">Update</button>
+        <button @click="deleteContact(key)">Delete</button>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
+import * as firebase from 'firebase'
+var firebaseConfig = {
+    apiKey: "AIzaSyBihnRYIuEePsdLR_X-ZtUcLFHmJfvFEuc",
+    authDomain: "ppproject-19364.firebaseapp.com",
+    databaseURL: "https://ppproject-19364.firebaseio.com",
+    projectId: "ppproject-19364",
+    storageBucket: "ppproject-19364.appspot.com",
+    messagingSenderId: "854439128758",
+    appId: "1:854439128758:web:71e000a4edeee5215dc017",
+    measurementId: "G-4J0E00B2FM"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+var database = firebase.database()
+var contactRef = database.ref('/contacts')
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data () {
+    return {
+      contacts: {},
+      tel: '',
+      name: '',
+      gunclass:'',
+      typegun:'',
+      updateTel: '',
+      updateName: '',
+      updateKey: '',
+      updateGunclass: '',
+      updateTypegun: '',
+    }
+  },
+  methods: {
+    insertToContact (tel, name,gunclass,typegun) {
+      let data = {
+        tel: tel,
+        name: name,
+        gunclass: gunclass,
+        typegun: typegun
+      }
+      contactRef.push(data)
+      this.tel = ''
+      this.name = ''
+      this.gunclass = ''
+      this.typegun = ''
+    },
+    setUpdateContact (key, contact) {
+      this.updateKey = key
+      this.updateTel = contact.tel
+      this.updateName = contact.name
+      this.updateGunclass = contact.gunclass
+      this.updateTypegun = contact.typegun
+    },
+    updateContact (tel, name,gunclass,typegun) {
+      contactRef.child(this.updateKey).update({
+        tel: tel,
+        name: name,
+        gunclass: gunclass,
+        typegun: typegun
+      })
+      this.updateKey = ''
+      this.updateTel = ''
+      this.updateName = ''
+      this.updateGunclass = ''
+      this.updateTypegun = ''
+    },
+    deleteContact (key) {
+      contactRef.child(key).remove()
+    }
+  },
+  mounted () {
+    contactRef.on('value', (snapshot) => {
+      this.contacts = snapshot.val()
+    })
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+h1, h2 {
+  font-weight: normal;
 }
 ul {
   list-style-type: none;
---
+++ src/components/User.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="user">
+    <h2>{{ name }}</h2>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'User',
+    props: {
+      name: String,
+      bio: String
+    }
+  }
+</script>
+
+<style>
+
+</style>
